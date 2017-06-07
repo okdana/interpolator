4bc2772d93c31d5f6366765aee001c28585e786f
dana <dana@dana.is>
2017-06-07 01:25:18 -0500

    Significantly improve functionality — user-specified filters, auto filters, &c.

--- src/Interpolator.php
+++ src/Interpolator.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Dana\Interpolator.
  *
- * @author  dana geier <dana@dana.is>
+ * @author  dana <dana@dana.is>
  * @license MIT
  */
 
@@ -11,50 +11,150 @@ namespace Dana\Interpolator;
 
 class Interpolator {
 	/**
-	 * @var array Default instance options.
-	 */
-	protected $defaultOptions = [
-		// If true, throw an exception when the referenced fixture key does not
-		// exist or the fixture value could not be coerced to a string;
-		// otherwise, silently replace the value by an empty string
-		'strict' => true,
-	];
-
-	/**
 	 * @var array Current instance options.
 	 */
 	protected $options = [];
+	/**
+	 * @var array Current instance filters.
+	 */
+	protected $filters = [];
+	/**
+	 * @var array Current instance auto filters.
+	 */
+	protected $autoFilters = [];
 
 	/**
 	 * Constructor.
 	 *
-	 * @param array $options (optional) Any instance options to set.
+	 * @param null|array $options
+	 *   (optional) Any instance options to set (see setOptions()).
+	 *
+	 * @param null|array $filters
+	 *   (optional) Any instance filters to set (see setFilters()).
+	 *
+	 * @param null|string|array $autoFilters
+	 *   (optional) Any specifiers for filters to apply automatically (see
+	 *   setAutoFilters()).
 	 *
 	 * @return self
 	 */
-	public function __construct(array $options = []) {
-		$this->setOptions();
+	public function __construct(
+		$options     = null,
+		$filters     = null,
+		$autoFilters = null
+	) {
 		$this->setOptions($options);
+		$this->setFilters($filters);
+		$this->setAutoFilters($autoFilters);
+	}
+
+	/**
+	 * Returns the default instance options as an associative array.
+	 *
+	 * @return array
+	 */
+	public function getDefaultOptions() {
+		return ['strict' => true];
+	}
+
+	/**
+	 * Returns the default instance filters as an associative array.
+	 *
+	 * @return array
+	 */
+	public function getDefaultFilters() {
+		return [
+			// Remove all non-alphabetical characters
+			'a' => function ($str) {
+				return preg_replace('/[^A-Za-z]/u', '', $str);
+			},
+			// Encode with base64
+			'b' => 'base64_encode',
+			// Encode with 'URL-safe' base64
+			'B' => function ($str) {
+				return rtrim(strtr(base64_encode($str), '/+', '_-'), '=');
+			},
+			// Hash with CRC32
+			'c' => function ($str) {
+				return hash('crc32', $str);
+			},
+			// Hash with CRC32b
+			'C' => function ($str) {
+				return hash('crc32b', $str);
+			},
+			// Remove all non-numeric characters
+			'd' => function ($str) {
+				return preg_replace('/[^0-9]/u', '', $str);
+			},
+			// Escape for shell
+			'e' => 'escapeshellarg',
+			// Escape for URL (RFC3986), but keep forward-slashes
+			'f' => function ($str) {
+				return implode('/', array_map('rawurlencode', explode('/', $str)));
+			},
+			// Escape for HTML
+			'h' => 'htmlspecialchars',
+			// Escape for HTML (all possible entities)
+			'H' => 'htmlentities',
+			// Escape for JSON
+			'j' => 'json_encode',
+			// Convert to lower-case (Unicode-safe)
+			'l' => 'mb_strtolower',
+			// Convert to lower-case
+			'L' => 'strtolower',
+			// Hash with MD5
+			'm' => function ($str) {
+				return hash('md5', $str);
+			},
+			// Escape for PCRE pattern
+			'p' => 'preg_quote',
+			// Escape for URL (RFC3986)
+			'r' => 'rawurlencode',
+			// Escape for URL (RFC1866)
+			'R' => 'urlencode',
+			// Hash with SHA1
+			's' => function ($str) {
+				return hash('sha1', $str);
+			},
+			// Hash with SHA256
+			'S' => function ($str) {
+				return hash('sha256', $str);
+			},
+			// Trim leading/trailing white space
+			't' => 'trim',
+			// Convert to upper-case (Unicode-safe)
+			'u' => 'mb_strtoupper',
+			// Convert to upper-case
+			'U' => 'strtoupper',
+			// Collapse consecutive white space
+			'w' => function ($str) {
+				return preg_replace('/\s+/u', ' ', $str);
+			},
+			// Remove all white space
+			'W' => function ($str) {
+				return preg_replace('/\s+/u', '', $str);
+			},
+		];
 	}
 
 	/**
 	 * Sets an array of instance options.
 	 *
-	 * @param array $options
-	 *   (optional) An array of instance options to set. If empty, all options
-	 *   will be over-written by their defaults.
+	 * @param null|array $options
+	 *   (optional) An array of instance options to set. If null or empty, all
+	 *   options will be over-written by their defaults.
 	 *
 	 * @return self
 	 */
-	public function setOptions(array $options = []) {
+	public function setOptions($options = null) {
 		if ( empty($options) ) {
-			$this->setOptions($this->defaultOptions);
-			return $this;
+			return $this->setOptions($this->getDefaultOptions());
 		}
 
 		foreach ( $options as $name => $value ) {
 			$this->setOption($name, $value);
 		}
+
 		return $this;
 	}
 
@@ -69,7 +169,7 @@ class Interpolator {
 	 * @throws \InvalidArgumentException if $name is unrecognised.
 	 */
 	public function setOption($name, $value) {
-		if ( ! array_key_exists($name, $this->defaultOptions) ) {
+		if ( ! array_key_exists($name, $this->getDefaultOptions()) ) {
 			throw new \InvalidArgumentException("Unrecognised option: ${name}");
 		}
 
@@ -99,65 +199,151 @@ class Interpolator {
 		if ( ! array_key_exists($name, $this->options) ) {
 			throw new \InvalidArgumentException("Unrecognised option: ${name}");
 		}
+
 		return $this->options[$name];
 	}
 
 	/**
-	 * Applies filters to a string.
+	 * Registers an array of filters.
 	 *
-	 * @param string $string  The string being substituted.
-	 * @param array  $filters Zero or more filter specifiers.
+	 * @param null|array $options
+	 *   (optional) An associative array of filters to set, where each key is a
+	 *   single ASCII letter and each value is a callable. If null, the default
+	 *   filters will be registered. If empty, all filters will be unregistered.
 	 *
-	 * @return string The (possibly) filtered string.
-	 *
-	 * @throws \RuntimeException if a supplied filter specifier is unrecognised.
+	 * @return self
 	 */
-	protected function applyFilters($string, array $filters) {
-		foreach ( $filters as $filter ) {
-			switch ( $filter ) {
-				case 'e':
-					$string = escapeshellarg($string);
-					break;
-				case 'h':
-					$string = htmlspecialchars($string);
-					break;
-				case 'H':
-					$string = htmlentities($string);
-					break;
-				case 'j':
-					$string = json_encode($string);
-					break;
-				case 'L':
-					$string = mb_strtolower($string);
-					break;
-				case 'p':
-					$string = preg_quote($string);
-					break;
-				case 'r':
-					$string = urlencode($string);
-					break;
-				case 'R':
-					$string = rawurlencode($string);
-					break;
-				case 't':
-					$string = trim($string);
-					break;
-				case 'U':
-					$string = mb_strtoupper($string);
-					break;
-				case 'w':
-					$string = preg_replace('/\s+/', ' ', $string);
-					break;
-				case 'W':
-					$string = preg_replace('/\s+/', '', $string);
-					break;
-				default:
-					throw new \RuntimeException(
-						"Unrecognised filter specifier: ${filter}"
-					);
+	public function setFilters($filters = null) {
+		if ( $filters === null ) {
+			return $this->setFilters($this->getDefaultFilters());
+		} elseif ( empty($filters) ) {
+			$this->filters = [];
+			return $this;
+		}
+
+		foreach ( $filters as $specifier => $callback ) {
+			$this->setFilter($specifier, $callback);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Registers a filter.
+	 *
+	 * @param string $specifier
+	 *   The single-letter specifier for the filter.
+	 *
+	 * @param callable $callable
+	 *   A callable which accepts as its first or only argument the string to be
+	 *   filtered.
+	 *
+	 * @return self
+	 *
+	 * @throws \InvalidArgumentException if $specifier is invalid.
+	 * @throws \InvalidArgumentException if $callable is invalid.
+	 */
+	public function setFilter($specifier, $callable) {
+		if ( strlen($specifier) !== 1 || ! preg_match('/^[A-Za-z]$/', $specifier) ) {
+			throw new \InvalidArgumentException("Illegal specifier: ${specifier}");
+		}
+
+		if ( ! is_callable($callable) ) {
+			throw new \InvalidArgumentException(sprintf(
+				'Expected callable (got %s)',
+				gettype($callable)
+			));
+		}
+
+		$this->filters[$specifier] = $callable;
+		return $this;
+	}
+
+	/**
+	 * Returns the current instance filters.
+	 *
+	 * @return array
+	 *   An associative array of filter specifiers and their callables.
+	 */
+	public function getFilters() {
+		return $this->filters;
+	}
+
+	/**
+	 * Returns the current value of an instance option.
+	 *
+	 * @param string $name The name of the option.
+	 *
+	 * @return mixed The value of the option.
+	 *
+	 * @throws \InvalidArgumentException if $specifier is unrecognised.
+	 */
+	public function getFilter($specifier) {
+		if ( ! isset($this->filters[$specifier]) ) {
+			throw new \InvalidArgumentException("Unrecognised specifier: ${specifier}");
+		}
+
+		return $this->filters[$specifier];
+	}
+
+	/**
+	 * Sets filters to be applied automatically.
+	 *
+	 * @param null|string|array $specifiers
+	 *   (optional) Zero or more valid specifiers corresponding to filters which
+	 *   should be applied automatically after all other filters have finished.
+	 *   For example, if 'u' is set as the auto filter, each replacement will be
+	 *   upper-cased after other filters have been applied. Auto filters can be
+	 *   suppressed on a per-place-holder basis using the special `-` specifier.
+	 *   Providing an empty $specifiers value disables auto filters.
+	 *
+	 * @return self
+	 *
+	 * @throws \InvalidArgumentException if one of $specifiers is unrecognised.
+	 */
+	public function setAutoFilters($specifiers = null) {
+		if ( $specifiers === null || $specifiers === '' || $specifiers === [] ) {
+			$this->autoFilters = [];
+			return $this;
+		}
+
+		if ( is_string($specifiers) ) {
+			$specifiers = str_split($specifiers);
+		}
+
+		foreach ( $specifiers as $specifier ) {
+			if ( ! isset($this->filters[$specifier]) ) {
+				throw new \InvalidArgumentException("Unrecognised specifier: ${specifier}");
 			}
 		}
-		return (string) $string;
+
+		$this->autoFilters = $specifiers;
+		return $this;
+	}
+
+	/**
+	 * Returns auto-filter specifiers as an array.
+	 *
+	 * @return array
+	 */
+	public function getAutoFilters() {
+		return $this->autoFilters;
+	}
+
+	/**
+	 * Renders a string via interpolation.
+	 *
+	 * @param string $string   The string to interpolate.
+	 * @param array  $fixtures The fixtures to use for interpolation.
+	 *
+	 * @return string The interpolated string.
+	 */
+	public function render($string, array $fixtures) {
+		// Hopefully a small optimisation (@todo: test this assumption)
+		if ( strpos($string, '%{') === false ) {
+			return $string;
+		}
+		return $this->interpolate($string, $fixtures);
 	}
 
 	/**
@@ -169,11 +355,11 @@ class Interpolator {
 	 * @return string The interpolated string.
 	 *
 	 * @throws \RuntimeException if strict and a fixture name is not found.
-	 * @throws \RuntimeException if strict and a fixture's type is illegal.
+	 * @throws \RuntimeException if strict and a fixture's type is unsupported.
 	 */
 	protected function interpolate($string, array $fixtures) {
 		return preg_replace_callback(
-			'/(\\\\*)(%\{(\w+)(?:\|([A-Za-z0-9]*))?\})/',
+			'/(\\\\*)(\%\{([\w.-]+)(?:\|([A-Za-z0-9-]*))?\})/',
 			function ($m) use ($fixtures) {
 				$backslashes = $m[1];
 				$placeholder = $m[2];
@@ -208,7 +394,7 @@ class Interpolator {
 						$replace = (string) $replace;
 					} elseif ( $this->options['strict'] ) {
 						throw new \RuntimeException(sprintf(
-							"Illegal fixture value type: %s (%s)",
+							"Unsupported fixture value type: %s (%s)",
 							$fixture,
 							gettype($replace)
 						));
@@ -226,19 +412,46 @@ class Interpolator {
 	}
 
 	/**
-	 * Renders a string via interpolation.
+	 * Applies filters to a string.
 	 *
-	 * @param string $string   The string to interpolate.
-	 * @param array  $fixtures The fixtures to use for interpolation.
+	 * @param string $string  The string being filtered/substituted.
+	 * @param array  $filters Zero or more filter specifiers.
 	 *
-	 * @return string The interpolated string.
+	 * @return string The (possibly) filtered string.
+	 *
+	 * @throws \RuntimeException if a supplied filter specifier is unrecognised.
 	 */
-	public function render($string, array $fixtures) {
-		// Hopefully a small optimisation (@todo: test this assumption)
-		if ( strpos($string, '%{') === false ) {
-			return $string;
+	protected function applyFilters($string, array $specifiers) {
+		$applyAutoFilters = true;
+
+		foreach ( $specifiers as $specifier ) {
+			if ( $specifier === '-' ) {
+				$applyAutoFilters = false;
+				continue;
+			}
+
+			if ( ! isset($this->filters[$specifier]) ) {
+				throw new \RuntimeException(
+					"Unrecognised or invalid filter specifier: ${specifier}"
+				);
+			}
+
+			$string = $this->filters[$specifier]($string);
 		}
-		return $this->interpolate($string, $fixtures);
+
+		if ( $applyAutoFilters && ! empty($this->autoFilters) ) {
+			foreach ( $this->autoFilters as $specifier ) {
+				if ( ! isset($this->filters[$specifier]) ) {
+					throw new \RuntimeException(
+						"Unrecognised or invalid filter specifier: ${specifier}"
+					);
+				}
+
+				$string = $this->filters[$specifier]($string);
+			}
+		}
+
+		return (string) $string;
 	}
 }
 
--- tests/InterpolatorTest.php
+++ tests/InterpolatorTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Dana\Interpolator.
  *
- * @author  dana geier <dana@dana.is>
+ * @author  dana <dana@dana.is>
  * @license MIT
  */
 
@@ -11,7 +11,7 @@ namespace Dana\Interpolator\Test;
 
 use Dana\Interpolator\Interpolator;
 
-class InterpolatorTest extends \PHPUnit_Framework_TestCase {
+class InterpolatorTest extends \PHPUnit\Framework\TestCase {
 	/**
 	 * Data provider for testRender().
 	 *
@@ -38,16 +38,28 @@ class InterpolatorTest extends \PHPUnit_Framework_TestCase {
 			[[], 'x%{0}',  [$test], 'x' . $test],
 			[[], '%{0}x',  [$test], $test . 'x'],
 			[[], 'x%{0}x', [$test], 'x' . $test . 'x'],
+			[[], '%{0|a}', [$test], 'foobar'], // lazy
+			[[], '%{0|b}', [$test], base64_encode($test)],
+			[[], '%{0|B}', [$test], rtrim(strtr(base64_encode($test), '/+', '_-'), '=')],
+			[[], '%{0|c}', [$test], hash('crc32', $test)],
+			[[], '%{0|C}', [$test], hash('crc32b', $test)],
+			[[], '%{0|d}', [$test], ''], // lazy
 			[[], '%{0|e}', [$test], escapeshellarg($test)],
+			[[], '%{0|f}', [$test], rawurlencode($test)], // lazy
 			[[], '%{0|h}', [$test], htmlspecialchars($test)],
 			[[], '%{0|H}', [$test], htmlentities($test)],
 			[[], '%{0|j}', [$test], json_encode($test)],
-			[[], '%{0|L}', [$test], mb_strtolower($test)],
+			[[], '%{0|l}', [$test], mb_strtolower($test)],
+			[[], '%{0|L}', [$test], strtolower($test)],
+			[[], '%{0|m}', [$test], hash('md5', $test)],
 			[[], '%{0|p}', [$test], preg_quote($test)],
-			[[], '%{0|r}', [$test], urlencode($test)],
-			[[], '%{0|R}', [$test], rawurlencode($test)],
+			[[], '%{0|r}', [$test], rawurlencode($test)],
+			[[], '%{0|R}', [$test], urlencode($test)],
+			[[], '%{0|s}', [$test], hash('sha1', $test)],
+			[[], '%{0|S}', [$test], hash('sha256', $test)],
 			[[], '%{0|t}', [$test], trim($test)],
-			[[], '%{0|U}', [$test], mb_strtoupper($test)],
+			[[], '%{0|u}', [$test], mb_strtoupper($test)],
+			[[], '%{0|U}', [$test], strtoupper($test)],
 			[[], '%{0|w}', [$test], preg_replace('/\s+/', ' ', $test)],
 			[[], '%{0|W}', [$test], preg_replace('/\s+/', '',  $test)],
 
@@ -58,6 +70,8 @@ class InterpolatorTest extends \PHPUnit_Framework_TestCase {
 			// Associative fixture tests
 			[[], '%{a}',   ['a'   => $test],             $test],
 			[[], '%{aaa}', ['aaa' => $test],             $test],
+			[[], '%{a.b}', ['a.b' => $test],             $test],
+			[[], '%{a-b}', ['a-b' => $test],             $test],
 			[[], '%{1a}',  ['1a'  => $test],             $test],
 			[[], '%{a1}',  ['a1'  => $test],             $test],
 			[[], '%{_}',   ['_'   => $test],             $test],
@@ -67,18 +81,26 @@ class InterpolatorTest extends \PHPUnit_Framework_TestCase {
 
 			// Non-strict-mode missing-fixture tests
 			[['strict' => false], '%{1}',    [$test], ''],
+			[['strict' => false], '%{1|b}',  [$test], base64_encode('')],
+			[['strict' => false], '%{1|c}',  [$test], hash('crc32', '')],
 			[['strict' => false], '%{1|e}',  [$test], escapeshellarg('')],
-			[['strict' => false], '%{1|h}',  [$test], htmlspecialchars('')],
-			[['strict' => false], '%{1|H}',  [$test], htmlentities('')],
+			[['strict' => false], '%{1|f}',  [$test], ''],
+			[['strict' => false], '%{1|h}',  [$test], ''],
+			[['strict' => false], '%{1|H}',  [$test], ''],
 			[['strict' => false], '%{1|j}',  [$test], json_encode('')],
-			[['strict' => false], '%{1|L}',  [$test], mb_strtolower('')],
-			[['strict' => false], '%{1|p}',  [$test], preg_quote('')],
-			[['strict' => false], '%{1|r}',  [$test], urlencode('')],
-			[['strict' => false], '%{1|R}',  [$test], rawurlencode('')],
-			[['strict' => false], '%{1|t}',  [$test], trim('')],
-			[['strict' => false], '%{1|U}',  [$test], mb_strtoupper('')],
-			[['strict' => false], '%{1|w}',  [$test], preg_replace('/\s+/', ' ', '')],
-			[['strict' => false], '%{1|W}',  [$test], preg_replace('/\s+/', '',  '')],
+			[['strict' => false], '%{1|l}',  [$test], ''],
+			[['strict' => false], '%{1|L}',  [$test], ''],
+			[['strict' => false], '%{1|m}',  [$test], hash('md5', '')],
+			[['strict' => false], '%{1|p}',  [$test], ''],
+			[['strict' => false], '%{1|r}',  [$test], ''],
+			[['strict' => false], '%{1|R}',  [$test], ''],
+			[['strict' => false], '%{1|s}',  [$test], hash('sha1', '')],
+			[['strict' => false], '%{1|S}',  [$test], hash('sha256', '')],
+			[['strict' => false], '%{1|t}',  [$test], ''],
+			[['strict' => false], '%{1|u}',  [$test], ''],
+			[['strict' => false], '%{1|U}',  [$test], ''],
+			[['strict' => false], '%{1|w}',  [$test], ''],
+			[['strict' => false], '%{1|W}',  [$test], ''],
 
 			// Non-strict-mode illegal-type tests
 			[['strict' => false], '%{0}', [[]],          'array'],
@@ -165,6 +187,59 @@ class InterpolatorTest extends \PHPUnit_Framework_TestCase {
 	 */
 	public function testRenderException($options, $string, $fixtures) {
 		(new Interpolator($options))->render($string, $fixtures);
+	}
+
+	/**
+	 * Tests filter registration.
+	 *
+	 * @return void
+	 */
+	public function testFilterRegistration() {
+		$interpolator = new Interpolator([], []);
+
+		$interpolator->setFilter('z', 'strtoupper');
+		$interpolator->setFilter('Z', function ($str) {
+			return strtoupper($str);
+		});
+
+		$this->assertSame('strtoupper', $interpolator->getFilter('z'));
+
+		$this->assertSame('FOO', $interpolator->render('%{0|z}', ['foo']));
+		$this->assertSame('FOO', $interpolator->render('%{0|Z}', ['foo']));
+
+		// Other filters should throw an exception, since we unset them
+		try {
+			$interpolator->render('%{0|h}', ['foo']);
+			$this->assertTrue(false);
+		} catch ( \Exception $e ) {
+			// pass
+		}
+	}
+
+	/**
+	 * Tests auto filters.
+	 *
+	 * @return void
+	 */
+	public function testAutoFilters() {
+		$interpolator = new Interpolator(null, null, 'ua');
+
+		$this->assertSame(
+			'FOOBAR',
+			$interpolator->render('%{0}', ['foo BAR'])
+		);
+		$this->assertSame(
+			'FOOBAR',
+			$interpolator->render('%{0|l}', ['foo BAR'])
+		);
+		$this->assertSame(
+			'foo BAR',
+			$interpolator->render('%{0|-}', ['foo BAR'])
+		);
+		$this->assertSame(
+			'foo bar',
+			$interpolator->render('%{0|l-}', ['foo BAR'])
+		);
 	}
 }
 
